b2ca8b488375d6f25b6c2ccac84f68a4e5631acc
sephirothz87 <sephiroth.zhong@gmail.com>
2015-08-19 15:49:41 +0800

    测试OS_PUSH的API的网站

---
+++ js/index.js
@@ -0,0 +1,36 @@
+(function() {
+    console.log("build 0003");
+
+    $(document).ready(function() {
+        $("#button").click(function() {
+            console.log("button be clicked");
+            $.ajax({
+                // url: "http://user.ospush.localhost/api/visit/lastVisit",
+                url: "http://user.ospush.localhost/api/news/unreadNewsIds",
+                type: "post",
+                dataType: "json",
+                // dataType: "jsonp",
+                // crossDomain: true,
+                // jsonp:"jsonpcallback",
+                data: {
+                    //19号用户
+                    app_token: "38fae4e03bca72d41ad51000fafe5a44",
+                    //28号用户
+                    // app_token: "ccebe01738f357c2122e6ec19d6d52ca",
+                    uuid: "11111111-1111-1111-1111-111111111111",
+                    push_token: "57258b2935656ed35788e9e6bcf2eeaaea2c5974b9838369e26c54fd7ecb0e06"
+                },
+                success: function(data) {
+                    console.log("success");
+                    var result = data.result;
+                    console.log(data);
+                },
+                error: function(err) {
+                    console.log("error");
+                    var errorCode = err.code;
+                    console.log(err);
+                }
+            });
+        })
+    });
+})();
