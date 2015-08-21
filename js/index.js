1adbfdb9216a5d0a6b9e9f03a0ae1fbe6fd6d22e
sephirothz87 <sephiroth.zhong@gmail.com>
2015-08-21 08:59:34 +0800

    测试pushAPI的代码

--- js/index.js
+++ js/index.js
@@ -6,19 +6,24 @@
             console.log("button be clicked");
             $.ajax({
                 // url: "http://user.ospush.localhost/api/visit/lastVisit",
-                url: "http://user.ospush.localhost/api/news/unreadNewsIds",
+                // url: "http://user.ospush.localhost/api/news/unreadNewsIds",
+                url: "http://user.ospush.localhost/api/push_send/sendMail",
                 type: "post",
                 dataType: "json",
                 // dataType: "jsonp",
                 // crossDomain: true,
                 // jsonp:"jsonpcallback",
                 data: {
+                    //API:unreadNewsIds
                     //19号用户
-                    app_token: "38fae4e03bca72d41ad51000fafe5a44",
+                    // app_token: "38fae4e03bca72d41ad51000fafe5a44",
                     //28号用户
                     // app_token: "ccebe01738f357c2122e6ec19d6d52ca",
-                    uuid: "11111111-1111-1111-1111-111111111111",
-                    push_token: "57258b2935656ed35788e9e6bcf2eeaaea2c5974b9838369e26c54fd7ecb0e06"
+                    // uuid: "11111111-1111-1111-1111-111111111111",
+                    // push_token: "57258b2935656ed35788e9e6bcf2eeaaea2c5974b9838369e26c54fd7ecb0e06"
+                    from: "ccebe01738f357c2122e6ec19d6d52ca",
+                    to: "11111111-1111-1111-1111-111111111111",
+                    msg: "57258b2935656ed35788e9e6bcf2eeaaea2c5974b9838369e26c54fd7ecb0e06"
                 },
                 success: function(data) {
                     console.log("success");
