7c6c7564b84561576dd0998038efe501f4af26ce
sephirothz87 <sephiroth.zhong@gmail.com>
2015-09-02 11:40:20 +0800

    合入testAPI的模版性改动

---
+++ js/index.js
@@ -0,0 +1,9 @@
+(function() {
+    console.log("build 0001");
+
+    $(document).ready(function() {
+        $("#button").click(function() {
+            console.log("button be clicked");
+        })
+    });
+})();
