fed858ce6127b5c46f01a3d9805516169a4184f9
unknown <sephiroth.zhong@gmail.com>
2015-09-22 17:26:02 +0800

    修复IOS无法输入的问题 增加步数大于30000不提交的功能

--- ledongli/js/index.js
+++ ledongli/js/index.js
@@ -1,5 +1,5 @@
 (function() {
-    console.log("ledongli build 0001");
+    // console.log("ledongli build 0001");
 
     $(document).ready(function() {
         $("#button").click(function() {
@@ -10,6 +10,12 @@
 
             console.log("l_id = "+l_id);
             console.log("steps = "+steps);
+
+            if(steps>30000){
+            	alert("steps>30000");
+            	return;
+            }
+
             console.log("time_stamp = "+time_stamp);
 
             var r_data="list=%5B%7B%22pm2d5%22%3A0%2C%22report%22%3A%22%5B%7B%5C%22activity%5C%22%3A%5C%22walking%5C%22%2C%5C%22calories%5C%22%3A83.321020733531611%2C%5C%22steps%5C%22%3A1840%2C%5C%22distance%5C%22%3A2402.7757200000001%2C%5C%22duration%5C%22%3A1260%7D%5D%22%2C%22distance%22%3A2402.7757200000001%2C%22steps%22%3A"+steps+"%2C%22date%22%3A"+time_stamp+"%2C%22calories%22%3A140.17265113023612%2C%22duration%22%3A1860%2C%22lon%22%3A121.54642728736559%2C%22activeValue%22%3A42868%2C%22lat%22%3A38.888830057174324%7D%5D&pc=47d98ceacc8876d4c4c93988cbb9b68150b51bc8";
