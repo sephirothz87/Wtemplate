4153dfaae019e2c9ed3291103b74476bf6b7147d
SephirothZ87 <sephiroth.zhong@gmail.com>
2015-11-25 17:29:09 +0800

    BAK 2015-11-25-172850 已经能取到比赛编号的值了

--- soccer/php/test.php
+++ soccer/php/test.php
@@ -11,7 +11,11 @@ include "../phpspider/phpspider/cls_query.php";
 include "../phpspider/user.php";
 include "../phpspider/phpspider/cls_curl.php";
 
+myPrint("start");
+
 $html;
+
+$date = "2014-11-22";
 
 $curl = new rolling_curl();
 $curl->set_gzip(true);
@@ -23,18 +27,43 @@ $curl->callback = function($response, $info, $request, $error) {
         myPrint("empty response");
     }
     else {
+        $res = array();
+        $res_array = array();
+
+        global $date;
+        $res['date'] = $date;
+        $res['info'] = "info";
+
         $html = $response;
         // myPrint($response);
         $parser = new simple_html_dom();
         $parser -> load($html);
-        $res = $parser -> find('table');
-        $res = $parser -> find('div[class=touzhu]');
-        myPrint($res);
+        // $res = $parser -> find('table');
+        $touzhus = $parser -> find('div[class=touzhu]');
+        myPrint("touzhus");
+        myPrint($touzhus);
 
+        $match = $touzhus[0] -> children(0);
+        myPrint("match");
+        myPrint($match);    
+
+        // $parser -> load($day_index_div);
+
+        // $day_index = $parser->find('span[class=xulie]');
+        $day_index = $match -> children(0)->children(0)->plaintext;
+        myPrint("day_index");
+        myPrint($day_index);
+
+        $res['day_index'] = $day_index;
+
+        $match_time = $match -> children(0)->children(0)->plaintext;
+
+        myPrint("res");
+        myPrint($res);
     }
 };
 
-$url = "http://www.okooo.com/jingcai/2014-11-22";
+$url = "http://www.okooo.com/jingcai/".$date;
 // $url = "http://cp.zgzcw.com/lottery/jchtplayvsForJsp.action?lotteryId=47&type=jcmini&issue=2014-11-22";
 
 $curl->get($url);
@@ -47,6 +76,8 @@ function myPrint ($arr)
     if (!is_array ($arr))
     {
             echo $arr.'<br/>';
+            fwrite(fopen( "log.txt" , "a" ), "[" . date('Y-m-d H:i:s' , time() + 60 * 60 * 6) . "]"  . $arr . "\n" );
+
         // return false;
         return;
     }
@@ -55,11 +86,15 @@ function myPrint ($arr)
     {
         if (is_array ($val))
         {
+            $str = "key = ".$key." value = ".$value;
             myPrint ($val);
         }
         else
        {
             echo $val.'<br/>';
+
+            $str = "key = ".$key." value = ".$val;
+            fwrite(fopen( "log.txt" , "a" ), "[" . date('Y-m-d H:i:s' , time() + 60 * 60 * 6) . "]"  . $str . "\n" );
         }
     }
 }
