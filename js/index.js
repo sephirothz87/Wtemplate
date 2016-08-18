e95dcdb3c12015e349d23eb163b0a86eb5b69d4d
zhong.zhicong <zhong.zhicong@newphoria.co.jp>
2016-08-18 14:35:13 +0800

    提取新的主页和子页模板

--- js/index.js
+++ js/index.js
@@ -1,9 +1,2 @@
-(function() {
-    console.log("build 0001");
-
-    $(document).ready(function() {
-        $("#button").click(function() {
-            console.log("button be clicked");
-        })
-    });
-})();
+$(function(){
+})
